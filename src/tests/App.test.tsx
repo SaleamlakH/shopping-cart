be585eccefa1a864f40a3c018a75ec221b97c193
SaleamlakH <saleamlak.7h@gmail.com>
2026-03-16 22:51:28 +0300

    refactor: update App component test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,10 +1,18 @@
 import { describe, test, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 import App from '../App';
 
 describe('Setup test', () => {
-  test('render Setup Heading', () => {
-    render(<App />);
-    expect(screen.getByText('Setup')).toBeInTheDocument();
+  test('render App and its children', () => {
+    // render(<RouterProvider router={router} />);
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+    expect(screen.getByRole('contentinfo')).toBeInTheDocument();
   });
 });
