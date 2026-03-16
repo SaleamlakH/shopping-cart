24335447c8508c0e817e6f4bd2ebae7d14ff9c00
SaleamlakH <saleamlak.7h@gmail.com>
2026-03-16 23:48:36 +0300

    refactor(Logo): set accessibility name explicitly to the link

--- src/layout/navigation/components/logo/Logo.tsx
+++ src/layout/navigation/components/logo/Logo.tsx
@@ -5,8 +5,8 @@ import logo from '/public/shopping-bag.svg';
 function Logo() {
   return (
     <div className={style.logo}>
-      <Link to="/">
-        <img src={logo} alt="Shopping cart logo" />
+      <Link to="/" aria-label="Shopping cart home">
+        <img src={logo} alt="" data-testid="logo-img" />
         <span className={style['logo-text']}>Shopping Cart</span>
       </Link>
     </div>
