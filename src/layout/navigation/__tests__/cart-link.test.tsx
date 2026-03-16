a8f7e0a809581592eaaaa1cdceef635c2e6c66bc
SaleamlakH <saleamlak.7h@gmail.com>
2026-03-16 23:37:11 +0300

    test: add test for CartLink

---
+++ src/layout/navigation/__tests__/cart-link.test.tsx
@@ -0,0 +1,36 @@
+import { describe, test, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import CartLink from '../components/cart-link/CartLink';
+import { MemoryRouter } from 'react-router';
+
+describe('cart Navigation link', () => {
+  test('render passed cart item quantity', () => {
+    render(
+      <MemoryRouter>
+        <CartLink quantity={2}></CartLink>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('2')).toBeInTheDocument();
+  });
+
+  test('link has correct reference', () => {
+    render(
+      <MemoryRouter>
+        <CartLink quantity={2} />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByRole('link')).toHaveAttribute('href', '/cart');
+  });
+
+  test('link has accessibility name', () => {
+    render(
+      <MemoryRouter>
+        <CartLink quantity={2} />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByRole('link', { name: /cart 2 items/i }));
+  });
+});
