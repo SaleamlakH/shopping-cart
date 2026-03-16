b1e195d0d30ed353cdbdbd64db917c2fc122158e
SaleamlakH <saleamlak.7h@gmail.com>
2026-03-16 23:47:49 +0300

    test: add test for Logo

---
+++ src/layout/navigation/__tests__/logo.test.tsx
@@ -0,0 +1,41 @@
+import { describe, test, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import Logo from '../components/logo/Logo';
+
+describe('logo component', () => {
+  test('render Logo', () => {
+    render(
+      <MemoryRouter>
+        <Logo />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByTestId('logo-img')).toBeInTheDocument();
+    expect(screen.getByRole('link')).toBeInTheDocument();
+    expect(screen.getByText(/shopping cart/i)).toBeInTheDocument();
+  });
+
+  test('the link has correct reference', () => {
+    render(
+      <MemoryRouter>
+        <Logo />
+      </MemoryRouter>
+    );
+
+    const link = screen.getByRole('link');
+    expect(link).toHaveAttribute('href', '/');
+  });
+
+  test('the link has accessibility name', () => {
+    render(
+      <MemoryRouter>
+        <Logo />
+      </MemoryRouter>
+    );
+
+    expect(
+      screen.getByRole('link', { name: /shopping cart home/i })
+    ).toBeInTheDocument();
+  });
+});
